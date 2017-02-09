8e33b2e06a37de3e0f862764098cf858dcc17775
Jordy <Jordy@Lenovo-Z510.wifi.hu.nl>
2017-02-09 10:22:17 +0100

    Kijk dit programma gaan dan, wajoo!

---
+++ Jaar2_Blok3/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String s = "Ha";
+		if(s.equals("Ha")) {
+			System.out.println("Magie");
+		}
+		else {
+			System.out.println("Geen magie, fuck jou");
+		}
+	}
+
+}
